8c6a0c8614a8589e0f5e11392aa9eb8237739475
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-06-10 18:32:53 -0400

    initial search working with NWS

---
+++ start/MyWeatherHub/Data/ForecastResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace MyWeatherHub.Data;
+
+public class ForecastResponse
+{
+    public string id { get; set; }
+    public string type { get; set; }
+    public Properties properties { get; set; }
+
+    public class Properties
+    {
+        public string geometry { get; set; }
+        public string zone { get; set; }
+        public DateTime updated { get; set; }
+        public Period[] periods { get; set; }
+    }
+
+    public class Period
+    {
+        public int number { get; set; }
+        public string name { get; set; }
+        public string detailedForecast { get; set; }
+    }
+}
---
+++ start/MyWeatherHub/Program.cs
@@ -0,0 +1,36 @@
+using MyWeatherHub.Components;
+using MyWeatherHub.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient<NwsManager>(client =>
+{
+	client.BaseAddress = new Uri("https://api.weather.gov/");
+	client.DefaultRequestHeaders.Add("User-Agent", "Microsoft - .NET Aspire Demo");
+});
+
+builder.Services.AddMemoryCache();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
